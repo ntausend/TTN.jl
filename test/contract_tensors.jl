d248f91e39e3bfe5f173051cc265e08ee5cf6f9e
Niklas Tausendpfund <n.tausendpfund@fz-juelich.de>
2022-11-15 18:30:52 +0100

    correct tests for contract_tensors

--- test/contract_tensors.jl
+++ test/contract_tensors.jl
@@ -1,7 +1,7 @@
 using TensorKit, TTNKit
 using Test
 
-@testset "Contraction TTN Network"
+@testset "Contraction TTN Network" begin
 
     n_layers = 2
     net = TTNKit.BinaryChainNetwork(n_layers)
@@ -9,7 +9,7 @@ using Test
     tensors = vcat(ttn.data...)
 
     inds = map(p -> TTNKit.internal_index_of_legs(net, p), TTNKit.NodeIterator(net))
-    unique_inds, cntr_tn = contract_tensors(tensors, inds)
+    unique_inds, cntr_tn = TTNKit.contract_tensors(tensors, inds)
 
 
     @tensor res_normal[-1,-2,-3,-4,-5] := tensors[1][-1,-2, 1] * tensors[2][-3,-4,2]*tensors[3][1,2,-5]
